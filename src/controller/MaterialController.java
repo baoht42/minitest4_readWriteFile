355322d3115be4e26daac1d49b9e0b01d01dd72a
baoht42 <snorlax4293@gmail.com>
2024-01-04 16:23:52 +0700

    add controller materialController

--- src/controller/MaterialController.java
+++ src/controller/MaterialController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import models.Material;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MaterialController {
+    List<Material> materialList = new ArrayList<>();
+
+    public MaterialController(List<Material> materialList) {
+        this.materialList = materialList;
+    }
+
+    public MaterialController() {
+    }
+    public void addNewMaterial(Material material){
+        materialList.add(material);
+    }
+    public void deleteMaterial(int index){
+        materialList.remove(index);
+    }
+
+    public List<Material> getMaterialList() {
+        return materialList;
+    }
+
+    public void setMaterialList(List<Material> materialList) {
+        this.materialList = materialList;
+    }
 }
